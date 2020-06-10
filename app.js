0d4282d99a0f1c50ef988d8ae6e4e00c34acf3af
qaqa313a <qaqa313a@gmail.com>
2020-06-11 00:14:27 +0900

    로그인 및 로그아웃 적용. 로그인 후 세션 관리

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var session = require('express-session');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,21 +8,26 @@ var logger = require('morgan');
 var mallRouter = require('./routes/mall');
 var usersRouter = require('./routes/users');
 var app = express();
-
+//session
+app.use(cookieParser());
+app.use(session({ 
+	secret: 'raining',
+	resave: false,
+	saveUninitialized: true
+}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/mall',express.static(path.join(__dirname, 'public')));
-
-
+app.use('/mall',express.static(path.join(__dirname, 'public'))); 
 app.use('/mall', mallRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/mall.js
+++ routes/mall.js
@@ -6,18 +6,54 @@ var pool = mysql.createPool({
 	connectionLimit: 5,
 	host: 'localhost',
 	user: 'root',
-	database: 'tutorial',
+	database: 'shopping',
 	password: 'gosemvhs1~@#'
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+   if(!req.session.logined){
+        req.session.logined = false;
+    }
+    console.log('확인:' + req.session.name + req.session.islogined);
+  res.render('index', { session : req.session });
 });
 
-/* GET home page. */
+/*로그인 get method*/
 router.get('/login', function(req, res, next) {
   res.render('login');
+});
+
+/*로그인 post method*/
+router.post('/login', function(req,res,next){
+	var email = req.body.email;
+	var user = req.body.user;
+	var password = req.body.password;
+	var datas = [email,password];
+
+	pool.getConnection(function(err, connection){
+		if(err) console.error("커넥션 객체 얻어오기 에러 : ",err);
+
+		/*user에 따른 조건문*/
+		if(user == 'administrator') var sql = "SELECT name FROM administrator where email=? and password=?";
+		else if(user == 'seller') var sql = "SELECT name FROM seller where email=? and password=?";
+		else var sql = "SELECT name FROM customer where email=? and password=?";
+		
+		connection.query(sql,datas, function(err, rows){
+			if(err) console.error(err);
+
+			req.session.name =rows[0].name; 
+			req.session.islogined = true;
+
+			res.redirect('/mall');
+			connection.release();
+		});
+	});
+});
+
+router.get('/logout', function(req, res, next) {
+	req.session.destroy();
+	res.redirect('/mall');
 });
 
 module.exports = router;

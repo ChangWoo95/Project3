3640606d8189ed0b017df441ad2855a7eb2ad329
Sang-Wook <sangwook1223@hanmail.net>
2020-06-16 23:59:33 +0900

    mall.js 커밋

--- routes/mall.js
+++ routes/mall.js
@@ -19,7 +19,7 @@ var pool = mysql.createPool({
 	host: 'localhost',
 	user: 'root',
 	database: 'shopping',
-	password: 'gosemvhs1~@#',
+	password: 'sw8836^^',
 	dateStrings: 'date',
 	multipleStatements: true
 });
